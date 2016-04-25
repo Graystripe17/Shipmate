4e07dfc21c1040a8ed0c4eda9628056d4980f326
Cathy Cushy <hakuna.matata.kitty@gmail.com>
2016-04-24 22:46:01 -0500

    Hm maybe android isn't so bad after all. Don't worry, be happy

--- app/src/main/java/com/cinnamint/shipmate/IncomingFragment.java
+++ app/src/main/java/com/cinnamint/shipmate/IncomingFragment.java
@@ -1,14 +1,29 @@
 package com.cinnamint.shipmate;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class IncomingFragment extends Fragment {
     public IncomingFragment() {
-
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.incoming_frag, container, false);
 
+        //(RecyclerView)rootView.findViewById(R.id.recycler1);
 
+        return rootView;
+    }
 }
